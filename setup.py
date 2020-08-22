64f746c95d3897d78f7deeac51cb6a10c7232e8c
LightningWB <63766407+LightningWB@users.noreply.github.com>
2020-08-22 15:28:35 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='travelersApi',
+      version='1.0',
+      description='Python API for thetravelers.online',
+      author='LightningWB',
+      url='https://github.com/LightningWB/thetravelers.online-API'
+      #packages=['selenium']
+     )
