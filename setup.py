b4af9807a9152441d51ca439c19effdbb0bc295e
LightningWB <63766407+LightningWB@users.noreply.github.com>
2020-08-22 17:31:05 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
-setup(name='travelersApi',
-      version='1.0',
+setup(name='thetravelers.online Api',
+      version='1.0.2',
       description='Python API for thetravelers.online',
+      long_description='An API for thetravelers.online written in python using a headless firefox. You need firefox installed first.',
       author='LightningWB',
-      url='https://github.com/LightningWB/thetravelers.online-API'
-      #packages=['selenium']
+      url='https://github.com/LightningWB/thetravelers.online-API',
+      packages=['travelersApi'],
+      install_requires=['selenium']
      )
